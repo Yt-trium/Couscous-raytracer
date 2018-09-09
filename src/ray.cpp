58e8b531d34e286ba104f837316feae2911f3865
Yt-trium <cyril.meyer@etu.unistra.fr>
2018-09-09 16:09:03 +0200

    basic Ray class

--- src/ray.cpp
+++ src/ray.cpp
@@ -2,5 +2,27 @@
 
 Ray::Ray()
 {
+    A = glm::vec3();
+    B = glm::vec3();
+}
 
+Ray::Ray(const glm::vec3 &a, const glm::vec3 &b)
+{
+    A = a;
+    B = b;
+}
+
+glm::vec3 Ray::origin() const
+{
+    return A;
+}
+
+glm::vec3 Ray::direction() const
+{
+    return B;
+}
+
+glm::vec3 Ray::pointAtParameter(float t) const
+{
+    return A + t*B;
 }
--- src/ray.h
+++ src/ray.h
@@ -1,11 +1,24 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include<glm/vec3.hpp>
+
+using glm::vec3;
 
 class Ray
 {
 public:
     Ray();
+    Ray(const vec3& a, const vec3& b);
+
+    vec3 origin() const;
+    vec3 direction() const;
+    vec3 pointAtParameter(float t) const;
+
+private:
+    vec3 A;
+    vec3 B;
+
 };
 
 #endif // RAY_H
