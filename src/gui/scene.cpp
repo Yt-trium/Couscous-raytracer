1fd26ec169fad26d463367d85e1a35de611e9683
Yt-trium <cyril.meyer.68@gmail.com>
2018-10-12 11:31:51 +0200

    work on Scene

--- src/gui/scene.cpp
+++ src/gui/scene.cpp
@@ -1,6 +1,24 @@
 #include "scene.h"
 
-Scene::Scene()
-{
+using namespace std;
+using namespace glm;
 
+void SceneObject::add_object(MeshGroup &world)
+{
+    mat4 transform;
+    transform = translate(mat4(1.0f), m_translate);
+    transform = rotate(transform, radians(m_rotate_d), m_rotate);
+    transform = scale(transform, m_scale);
+
+    shared_ptr<Material> mat_dft(new Lambertian(vec3(0.73f, 0.73f, 0.73f)));
+
+    switch(m_type)
+    {
+    case PLANE:
+        create_plane(world, mat_dft, transform);
+        break;
+    case CUBE:
+        create_cube(world, mat_dft, transform);
+        break;
+    }
 }
--- src/gui/scene.h
+++ src/gui/scene.h
@@ -1,20 +1,43 @@
 #ifndef SCENE_H
 #define SCENE_H
 
-#include <vector>
+// couscous includes.
+#include "renderer/visualobject.h"
+#include "renderer/material.h"
+
+// glm includes.
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/glm.hpp>
+
+// Standard includes.
+#include <string>
+#include <vector>
 
 enum object_type {PLANE, CUBE};
 
 class SceneObject
 {
 public:
-    SceneObject();
+    SceneObject(std::string n, glm::vec3 t, glm::vec3 r, float rd, glm::vec3 s, object_type ot, std::string m)
+        : m_name(n)
+        , m_translate(t)
+        , m_rotate(r)
+        , m_rotate_d(rd)
+        , m_scale(s)
+        , m_type(ot)
+        , m_material(m)
+    {}
 
-    glm::vec3 translate;
-    glm::vec3 rotate;
-    glm::vec3 scale;
-    object_type type;
+    void add_object(MeshGroup& world);
+
+private:
+    std::string m_name;
+    glm::vec3 m_translate;
+    glm::vec3 m_rotate;
+    float m_rotate_d;
+    glm::vec3 m_scale;
+    object_type m_type;
+    std::string m_material;
 };
 
 class SceneMaterial
@@ -22,6 +45,8 @@ class SceneMaterial
 public:
     SceneMaterial();
 
+private:
+    std::string name;
     glm::vec3 color;
     glm::vec3 emission;
 };
@@ -30,6 +55,7 @@ class Scene
 {
 public:
     Scene();
+
 
     std::vector<SceneMaterial> materials;
     std::vector<SceneObject> objects;
